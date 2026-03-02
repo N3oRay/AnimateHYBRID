f6b3c1589f421e75e165b761bf2209bf6a7b9cfe
N3oRay <serge.lopes.94@free.fr>
2026-03-02 21:51:59 +0100

    Add files via upload
    transition_frames = cfg.get("transition_frames", 8)
    transition_zoom = cfg.get("transition_zoom", 0.0)

--- scripts/n3rfastmovie.py
+++ scripts/n3rfastmovie.py
@@ -82,7 +82,9 @@ def main(args):
     dtype = torch.float16 if args.fp16 else torch.float32
 
     fps = cfg.get("fps", 12)
-    transition_zoom = cfg.get("transition_zoom", 0.0) # new param
+    # new param
+    transition_frames = cfg.get("transition_frames", 8)
+    transition_zoom = cfg.get("transition_zoom", 0.0)
     num_fraps_per_image = cfg.get("num_fraps_per_image", 12)
     transition_frames = cfg.get("transition_frames", 8)
 
